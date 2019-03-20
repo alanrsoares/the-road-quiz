1d6bb7197d0f7f6999d868569bc0f54e2caae039
Alan Soares <alanrsoars@gmail.com>
2019-03-21 09:44:07 +1300

    extract theme to theme.ts

--- src/ui/theme/index.ts
+++ src/ui/theme/index.ts
@@ -2,27 +2,7 @@ import baseStyled, {
   ReactNativeStyledInterface
 } from "styled-components/native";
 
-export const colors = {
-  primary: "#FFD400",
-  secondary: "#3482B9",
-  positive: "#49C04A",
-  negative: "#D03930",
-  neutral: "#AAA",
-  muted: "#EAEAEA",
-  hotpink: "#FC8BA4",
-  black: "#333",
-  white: "#FFF",
-  gray: "#CCC"
-};
-
-export const radius = {
-  default: 4
-};
-
-export const theme = {
-  colors,
-  radius
-};
+import * as theme from "./theme";
 
 export type Theme = typeof theme;
 export const styled = baseStyled as ReactNativeStyledInterface<Theme>;
---
+++ src/ui/theme/theme.ts
@@ -0,0 +1,26 @@
+export const colors = {
+  primary: "#FFD400",
+  secondary: "#3482B9",
+  positive: "#49C04A",
+  negative: "#D03930",
+  neutral: "#AAA",
+  muted: "#EAEAEA",
+  hotpink: "#FC8BA4",
+  black: "#333",
+  white: "#FFF",
+  gray: "#CCC"
+};
+
+const baseRadius = 4;
+
+export const radius = {
+  default: baseRadius
+};
+
+const baseFontSize = 18;
+
+export const fontSizes = {
+  default: baseFontSize,
+  small: baseFontSize * 0.8,
+  large: baseFontSize * 1.2
+};
