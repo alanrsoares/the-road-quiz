3efe0bd2358e7b44cd98f67368952d13ccbf2ca9
Alan Soares <alanrsoars@gmail.com>
2019-04-02 14:08:58 +1300

    build n stuff

--- android/app/src/main/java/com/theroadquiz/MainApplication.java
+++ android/app/src/main/java/com/theroadquiz/MainApplication.java
@@ -6,7 +6,7 @@ import com.facebook.react.ReactApplication;
 import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
 import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
 import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
-import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
+
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,13 +25,12 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appCenterCrashes_whenToSendCrashes)),
-            new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appCenterAnalytics_whenToEnableAnalytics)),
-            new AppCenterReactNativePackage(MainApplication.this),
-            new AsyncStoragePackage()
-      );
+      return Arrays.<ReactPackage>asList(new MainReactPackage(),
+          new AppCenterReactNativeCrashesPackage(MainApplication.this,
+              getResources().getString(R.string.appCenterCrashes_whenToSendCrashes)),
+          new AppCenterReactNativeAnalyticsPackage(MainApplication.this,
+              getResources().getString(R.string.appCenterAnalytics_whenToEnableAnalytics)),
+          new AppCenterReactNativePackage(MainApplication.this));
     }
 
     @Override
--- src/lib/Storage.ts
+++ src/lib/Storage.ts
@@ -1,4 +1,4 @@
-import AsyncStorage from "@react-native-community/async-storage";
+import { AsyncStorage } from "react-native";
 
 const LS_KEY = "@THE_ROAD_QUIZ";
 
