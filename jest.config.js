6a27cbb81032d39087afa8db73a42920ae341db8
Alan Soares <alanrsoars@gmail.com>
2019-04-11 13:11:12 +1200

    add ts-jest support to detox

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,20 @@
+// jest.config.js
+import { defaults as tsjPreset } from "ts-jest/presets";
+
 module.exports = {
+  ...tsjPreset,
   preset: "react-native",
+  transform: {
+    ...tsjPreset.transform,
+    "\\.js$": "<rootDir>/node_modules/react-native/jest/preprocessor.js"
+  },
+  globals: {
+    "ts-jest": {
+      babelConfig: true,
+      tsConfig: "tsconfig.jest.json"
+    }
+  },
+  cacheDirectory: ".jest/cache",
+  testPathIgnorePatterns: ["<rootDir>/e2e/", "<rootDir>/node_modules/"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
 };
